f5f58115f9ebc2a462a1b4bc690182cc8e2a1cd9
yugoo90 <uosuji54@gmail.com>
2023-06-12 10:04:32 -0600

    Finally got the pacman image on the screen.

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char **argv){
     constexpr std::size_t KMsPerFrame{1000 / KFramesPerSecond};
     constexpr std::size_t KScreenWidth{640};
     constexpr std::size_t KScreenHeight{640};
-    constexpr std::size_t KGridWidth{32};
-    constexpr std::size_t KGridHeight{32};
+    constexpr std::size_t KGridWidth{150};
+    constexpr std::size_t KGridHeight{150};
 
     Renderer renderer(KScreenWidth, KScreenHeight, KGridWidth, KGridHeight);
     Pacman pac (KGridHeight, KGridWidth, "./pacman.png");
